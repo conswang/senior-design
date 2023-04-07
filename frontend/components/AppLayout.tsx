9d0368164a982ca6585db40cb4b1a8e5a358f035
Constance Wang <constancezwang@gmail.com>
2023-04-07 03:44:19 -0400

    some info

--- frontend/components/AppLayout.tsx
+++ frontend/components/AppLayout.tsx
@@ -1,12 +1,19 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
 import { ReactNode } from "react";
 
 interface LayoutProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
-export default function AppLayout({children}: LayoutProps) {
-  return <Layout>
-    <Layout.Content style={{padding: "0 20%"}}>{children}</Layout.Content>
-  </Layout>
+export default function AppLayout({ children }: LayoutProps) {
+  return (
+    <Layout>
+      <Layout.Header style={{ padding: "0 20%" }}>
+        <Typography.Title style={{color: "white", margin: 0, paddingTop: 12}} level={2}>Donghua DB</Typography.Title>
+      </Layout.Header>
+      <Layout.Content style={{ padding: "0 20%", paddingBottom: "24px" }}>
+        {children}
+      </Layout.Content>
+    </Layout>
+  );
 }
--- frontend/components/ShowDetail.tsx
+++ frontend/components/ShowDetail.tsx
@@ -1,5 +1,5 @@
-import { Donghua } from "@prisma/client";
-import { Col, Row, Space, Image, Typography } from "antd";
+import { Donghua, Donghua_platform } from "@prisma/client";
+import { Col, Row, Space, Image, Typography, Divider } from "antd";
 import pinyin from "pinyin";
 
 interface ShowDetailProps {
@@ -22,8 +22,78 @@ const getDisplayTitle = (donghua: Donghua) => {
   return "Unknown Title";
 };
 
+const getDisplayPlatform = (platform: Donghua_platform | null): string => {
+  if (!platform) {
+    return "Unknown";
+  }
+  switch (platform) {
+    case "TV":
+      return "TV";
+    case "Web":
+      return "Web";
+    case "Unknown":
+      return "Unknown";
+  }
+};
+
 export default function ShowDetail({ donghua }: ShowDetailProps) {
   const displayTitle = getDisplayTitle(donghua);
+
+  let leftColumnInfo: Array<{ name?: string; value: string }> = [
+    {
+      value: donghua.titleChinese!,
+    },
+  ];
+
+  if (donghua.platform !== "Unknown") {
+    leftColumnInfo.push({
+      name: "Platform",
+      value: getDisplayPlatform(donghua.platform),
+    });
+  }
+  if (donghua.numEpisodes) {
+    leftColumnInfo.push({
+      name: "Episodes",
+      value:
+        donghua.numEpisodes.toString() +
+        (donghua.episodeLength ? ` episodes / ${donghua.episodeLength}` : ""),
+    });
+  }
+  // TODO: combine start/end dates into 1 field
+  if (donghua.startDate && donghua.startDate != "1900-01-01") {
+    leftColumnInfo.push({
+      name: "From",
+      value: donghua.startDate,
+    });
+  }
+  if (donghua.endDate && donghua.endDate != "2100-01-01") {
+    leftColumnInfo.push({
+      name: "To",
+      value: donghua.endDate,
+    });
+  }
+  if (donghua.source) {
+    leftColumnInfo.push({
+      name: "Source",
+      value: donghua.source,
+    });
+  }
+
+  leftColumnInfo.push({
+    name: "NSFW",
+    value: donghua.nsfw ? "yes" : "no",
+  });
+
+  const leftColumnElements = leftColumnInfo.map((info, index) => {
+    return (
+      <div key={index}>
+        {info.name && (
+          <Typography.Text strong>{`${info.name}:`}</Typography.Text>
+        )}
+        <Typography.Text>{`\t${info.value}`}</Typography.Text>
+      </div>
+    );
+  });
 
   return (
     <>
@@ -31,9 +101,23 @@ export default function ShowDetail({ donghua }: ShowDetailProps) {
       <Row>
         <Col span={6}>
           <Image src={donghua.imageUrl} width={225} preview={false} />
+          <Divider />
+          <Space direction="vertical" size="small">
+            {leftColumnElements}
+          </Space>
         </Col>
         <Col span={18}>
-          <Space direction="vertical" style={{ width: "100%" }}></Space>
+          <Space
+            direction="vertical"
+            style={{ width: "100%", padding: "0 24px" }}
+          >
+            <div>
+              <Typography.Title level={3}>Synopsis</Typography.Title>
+              <Typography.Paragraph>
+                {donghua.summaryEnglish}
+              </Typography.Paragraph>
+            </div>
+          </Space>
         </Col>
       </Row>
     </>
