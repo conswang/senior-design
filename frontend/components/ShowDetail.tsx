d16d8cc902e923c72cc91cf13fe1d787b3f14a6c
Constance Wang <constancezwang@gmail.com>
2023-04-21 05:59:31 -0400

    display more titles

--- frontend/components/ShowDetail.tsx
+++ frontend/components/ShowDetail.tsx
@@ -1,6 +1,12 @@
-import { Donghua, DonghuaTagCN, TagCN } from "@prisma/client";
+import { Donghua, DonghuaTagCN, Prisma, TagCN } from "@prisma/client";
 import { Col, Row, Space, Image, Typography, Divider, Tag } from "antd";
-import { getDisplayPlatform, getDisplaySummary, getDisplayTitle, placeHolderImage } from "./showUtil";
+import {
+  getDisplayChineseTitle,
+  getDisplayPlatform,
+  getDisplaySummary,
+  getDisplayTitle,
+  placeHolderImage,
+} from "./showUtil";
 
 interface ShowDetailProps {
   donghua: Donghua;
@@ -11,12 +17,7 @@ export default function ShowDetail({ donghua, tags }: ShowDetailProps) {
   const displayTitle = getDisplayTitle(donghua);
   const displaySummary = getDisplaySummary(donghua);
 
-  let leftColumnInfo: Array<{ name?: string; value: string }> = [
-    {
-      name: "Chinese title",
-      value: donghua.titleChinese!,
-    },
-  ];
+  let leftColumnInfo: Array<{ name?: string; value: string }> = [];
 
   if (donghua.platform !== "Unknown") {
     leftColumnInfo.push({
@@ -33,12 +34,16 @@ export default function ShowDetail({ donghua, tags }: ShowDetailProps) {
     });
   }
   // TODO: combine start/end dates into 1 field
-  const startDate = donghua.startDate && donghua.startDate != "1900-01-01" ? donghua.startDate : "??"
-  const endDate = donghua.endDate && donghua.endDate != "2100-01-01" ? donghua.endDate : "??"
+  const startDate =
+    donghua.startDate && donghua.startDate != "1900-01-01"
+      ? donghua.startDate
+      : "??";
+  const endDate =
+    donghua.endDate && donghua.endDate != "2100-01-01" ? donghua.endDate : "??";
   leftColumnInfo.push({
     name: "Air date",
-    value: `${startDate} to ${endDate}`
-  })
+    value: `${startDate} to ${endDate}`,
+  });
 
   if (donghua.source) {
     leftColumnInfo.push({
@@ -63,16 +68,37 @@ export default function ShowDetail({ donghua, tags }: ShowDetailProps) {
     );
   });
 
-  const tagList = tags.length > 0 ? tags.map((tag) => {
-    return <Tag key={tag.name}>{tag.nameEN}</Tag>
-  }) : <Typography.Text>No tags ¯\_(˶′◡‵˶)_/¯</Typography.Text>
+  const tagList =
+    tags.length > 0 ? (
+      tags.map((tag) => {
+        return <Tag key={tag.name}>{tag.nameEN}</Tag>;
+      })
+    ) : (
+      <Typography.Text>No tags ¯\_(˶′◡‵˶)_/¯</Typography.Text>
+    );
+
+  const synonyms =
+    (donghua.otherTitles! as Prisma.JsonArray).join(", ") ||
+    `No synonyms ¯\\_(　´∀｀)_/¯`;
 
   return (
     <Space direction="vertical" size="large" style={{ width: "100%" }}>
-      <Typography.Title>{displayTitle}</Typography.Title>
+      <div>
+        <Typography.Title>{displayTitle}</Typography.Title>
+        <Typography.Text
+          type="secondary"
+          style={{ marginTop: 0 }}
+        >{`${donghua.titleChinese!} / ${getDisplayChineseTitle(
+          donghua
+        )}`}</Typography.Text>
+      </div>
       <Row>
         <Col span={6}>
-          <Image src={donghua.imageUrl || placeHolderImage} width={225} preview={false} />
+          <Image
+            src={donghua.imageUrl || placeHolderImage}
+            width={225}
+            preview={false}
+          />
           <Divider />
           <Space direction="vertical" size="small">
             {leftColumnElements}
@@ -87,11 +113,11 @@ export default function ShowDetail({ donghua, tags }: ShowDetailProps) {
               <Typography.Title level={3} style={{ marginTop: 0 }}>
                 Synopsis
               </Typography.Title>
-              <Typography.Paragraph>
-                {displaySummary}
-              </Typography.Paragraph>
+              <Typography.Paragraph>{displaySummary}</Typography.Paragraph>
               <Typography.Title level={3}>Tags</Typography.Title>
               <Space wrap>{tagList}</Space>
+              <Typography.Title level={3}>Synonyms</Typography.Title>
+              <Typography.Text>{synonyms}</Typography.Text>
             </div>
           </Space>
         </Col>
--- frontend/components/showUtil.ts
+++ frontend/components/showUtil.ts
@@ -1,13 +1,13 @@
 import { Donghua, Donghua_platform } from "@prisma/client";
 import pinyin from "pinyin";
 
-export const getDisplayTitle = (donghua: Donghua) => {
+export const getDisplayTitle = (donghua: Donghua, showTones?: boolean) => {
   if (donghua.titleEnglish) {
     return donghua.titleEnglish;
   }
   if (donghua.titleChinese) {
     return pinyin(donghua.titleChinese, {
-      style: pinyin.STYLE_NORMAL,
+      style: showTones ? pinyin.STYLE_TONE : pinyin.STYLE_NORMAL,
     })
       .map(([word]) => {
         return [word.charAt(0).toUpperCase() + word.slice(1)];
@@ -15,6 +15,13 @@ export const getDisplayTitle = (donghua: Donghua) => {
       .join(" ");
   }
   return "Unknown Title";
+};
+
+export const getDisplayChineseTitle = (donghua: Donghua) => {
+  return pinyin(donghua.titleChinese!, { style: pinyin.STYLE_TONE }).map(([word]) => {
+    return [word.charAt(0).toUpperCase() + word.slice(1)];
+  })
+  .join(" ");
 };
 
 export const getDisplayPlatform = (
@@ -34,18 +41,19 @@ export const getDisplayPlatform = (
 };
 
 export const getDisplaySummary = (donghua: Donghua, limit?: number): string => {
-  let htmlStr = donghua.summaryEnglish || "No summary ¯\_(˶′◡‵˶)_/¯";
+  let htmlStr = donghua.summaryEnglish || "No summary ¯\\_(˶′◡‵˶)_/¯";
 
   if (limit && htmlStr.length > limit) {
     htmlStr = htmlStr.substring(0, limit) + "...";
   }
 
-  htmlStr = htmlStr.replace(/&lt;/g , "<");	 
-  htmlStr = htmlStr.replace(/&gt;/g , ">");     
-  htmlStr = htmlStr.replace(/&quot;/g , "\"");  
-  htmlStr = htmlStr.replace(/&#39;/g , "\'");   
-  htmlStr = htmlStr.replace(/&amp;/g , "&");
+  htmlStr = htmlStr.replace(/&lt;/g, "<");
+  htmlStr = htmlStr.replace(/&gt;/g, ">");
+  htmlStr = htmlStr.replace(/&quot;/g, '"');
+  htmlStr = htmlStr.replace(/&#39;/g, "'");
+  htmlStr = htmlStr.replace(/&amp;/g, "&");
   return htmlStr;
-}
+};
 
-export const placeHolderImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/White_Persian_Cat.jpg/220px-White_Persian_Cat.jpg";
+export const placeHolderImage =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/White_Persian_Cat.jpg/220px-White_Persian_Cat.jpg";
