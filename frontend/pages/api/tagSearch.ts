06a2436ff1b0979845b36172a5ee0560e3c0efbe
Constance Wang <constancezwang@gmail.com>
2023-04-27 14:39:24 -0400

    tag prefix search api

---
+++ frontend/pages/api/tagSearch.ts
@@ -0,0 +1,54 @@
+import { PrismaClient, TagCN } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export type TagSearchResData = {
+  tags: TagCN[];
+};
+
+// For autofill tags in UI
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<TagSearchResData | { error: string }>
+) {
+  if (req.method !== "GET") {
+    res.status(400).json({ error: "unaccepted request method, use GET" });
+    return;
+  }
+
+  if (typeof req.query.searchPrefix !== "string") {
+    res
+      .status(400)
+      .json({ error: "expected string type for searchPrefix query param" });
+    return;
+  }
+
+  const searchPrefix = req.query.searchPrefix;
+  if (searchPrefix == "") {
+    res.send({ tags: [] });
+  }
+
+  const prisma = new PrismaClient();
+
+  const tags = await prisma.tagCN.findMany({
+    where: {
+      OR: [
+        {
+          nameEN: {
+            startsWith: searchPrefix,
+          },
+        },
+        {
+          name: {
+            startsWith: searchPrefix,
+          }
+        },
+      ],
+    },
+    orderBy: {
+      count: "desc",
+    },
+    take: 10,
+  });
+
+  res.send({tags});
+}
